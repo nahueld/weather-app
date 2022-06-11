3f9a85cafe592b019bf9afcbc88278b1d65946e2
Nahuel Dealbera <nahuel.dealbera@gmail.com>
2022-06-11 13:02:51 -0300

    Improved cache

--- src/plugins/cache.js
+++ src/plugins/cache.js
@@ -2,41 +2,66 @@
 
 const fp = require('fastify-plugin')
 const NodeCache = require('node-cache')
+const uidSafe = require('uid-safe')
 
-/**
- * This plugins returns cached response from the only available route
- */
-module.exports = fp(async function (fastify, opts) {
+function onRequest (cache) {
+  return (request, reply, next) => {
+    if (request.method !== 'GET') return next()
+
+    const cacheControl = request.headers['cache-control'] || String()
+
+    const cacheControlOpts = cacheControl.split(' ')
+
+    if (cacheControlOpts.includes('no-cache')) return next()
+
+    const eTag = request.headers['if-none-match']
+
+    if (!eTag) return next()
+
+    const cachedValue = cache.get(eTag)
+
+    if (!cachedValue) return next()
+
+    return reply
+      .status(304)
+      .send()
+  }
+}
+
+function onSend (cache, maxAge) {
+  return (request, reply, payload, next) => {
+    if (request.method !== 'GET') return next()
+
+    if (reply.statusCode === 304) return next()
+
+    const eTag = uidSafe.sync(18)
+
+    cache.set(eTag, payload)
+
+    reply.header('cache-control', `max-age=${maxAge} s-maxage=${maxAge} public`)
+    reply.header('etag', eTag)
+
+    next()
+  }
+}
+
+async function cachePlugin (fastify, opts) {
   const { CACHE_MAX_AGE: maxAge } = opts
 
   const cache = new NodeCache({
     stdTTL: maxAge
   })
 
-  fastify.addHook('onRequest', (request, reply, next) => {
-    if (request.method !== 'GET') return next()
+  fastify.addHook('onRequest', onRequest(cache))
+  fastify.addHook('onSend', onSend(cache, maxAge))
+}
 
-    const identifier = Buffer.from(request.url).toString('base64')
-
-    const cachedValue = cache.get(identifier)
-
-    if (!cachedValue) {
-      return next()
-    }
-
-    return reply
-      .status(200)
-      .header('content-type', 'application/json')
-      .send(cachedValue)
-  })
-
-  fastify.addHook('onSend', (request, reply, payload, next) => {
-    if (request.method !== 'GET') return next()
-
-    const identifier = Buffer.from(request.url).toString('base64')
-
-    cache.set(identifier, payload)
-
-    next()
-  })
-})
+/**
+ * This plugins returns cached response from the only available route
+ */
+module.exports = {
+  onRequest,
+  onSend,
+  cachePlugin,
+  ...fp(cachePlugin)
+}
